ea68d3afad148b590fecc0a4074330ffb4af9199
Marie Katrine Ekeberg <mke@themkat.net>
2025-04-17 12:16:27 +0200

    Formatting of new text render module

--- src/text.rs
+++ src/text.rs
@@ -1,7 +1,10 @@
-use glium::{backend::Facade, glutin::api::cgl::display, index::NoIndices, uniform, Program, Surface, Texture2d, VertexBuffer};
-use rusttype::{point, Font, Scale};
+use glium::{
+    Blend, DrawParameters, Program, Surface, Texture2d, VertexBuffer, backend::Facade,
+    glutin::api::cgl::display, index::NoIndices, uniform,
+};
+use rusttype::{Font, Scale, point};
 
-use crate::geometry::{Vertex, SQUARE};
+use crate::geometry::{SQUARE, Vertex};
 
 const TEXT_RENDER_VERTEX_SHADER: &str = "#version 330 core
 
@@ -43,32 +46,46 @@ pub struct TextRenderer {
 
     // some utility caching to avoid recreating same text twice
     prev_text: Option<String>,
-    prev_texture: Option<Texture2d>
+    prev_texture: Option<Texture2d>,
 }
 
 impl TextRenderer {
-
     pub fn new<T: Facade>(display: &T) -> Self {
-        let program = Program::from_source(display, TEXT_RENDER_VERTEX_SHADER, TEXT_RENDER_FRAGMENT_SHADER, None).unwrap();
+        let program = Program::from_source(
+            display,
+            TEXT_RENDER_VERTEX_SHADER,
+            TEXT_RENDER_FRAGMENT_SHADER,
+            None,
+        )
+        .unwrap();
         let vertex_buffer = VertexBuffer::new(display, &SQUARE).unwrap();
         let index_buffer = NoIndices(glium::index::PrimitiveType::TriangleStrip);
-        
-        Self { font: Font::try_from_bytes(include_bytes!("../fonts/OpenSans-bold.ttf")).unwrap(), program, vertex_buffer, index_buffer, prev_text: None, prev_texture: None }
+
+        Self {
+            font: Font::try_from_bytes(include_bytes!("../fonts/OpenSans-bold.ttf")).unwrap(),
+            program,
+            vertex_buffer,
+            index_buffer,
+            prev_text: None,
+            prev_texture: None,
+        }
     }
-    
+
     pub fn render_text_to_texture<T: Facade>(&self, display: &T, text: &str) -> Texture2d {
         let scale = Scale::uniform(32.0);
         let glyphs = self.font.layout(text, scale, point(20.0, 20.0));
 
-        // TODO: process text. so we avoid too long text for single lines. 
-        
+        // TODO: process text. so we avoid too long text for single lines.
+
         // TODO: use glyphs to decide on a suitable size
         let mut result = vec![vec![(0, 0, 0, 0); 800]; 500];
         for glyph in glyphs {
             // only rasterize text that have data
             if let Some(bounding_box) = glyph.pixel_bounding_box() {
-                glyph.draw(|x,y,v| {
-                    result[(bounding_box.min.y as u32 + y) as usize][(bounding_box.min.x as u32 + x) as usize] = (255, 0, 255, (v*255.0) as u8);
+                glyph.draw(|x, y, v| {
+                    result[(bounding_box.min.y as u32 + y) as usize]
+                        [(bounding_box.min.x as u32 + x) as usize] =
+                        (255, 0, 255, (v * 255.0) as u8);
                 });
             }
         }
