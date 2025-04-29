bcfb7fa7247aa697a6ef3898a8cb9998127334b1
Marie Katrine Ekeberg <mke@themkat.net>
2025-04-29 18:06:52 +0200

    Remove unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-use std::{
-    io::{BufRead as _, BufReader, BufWriter, Read, Write},
-    net::TcpListener,
-};
-
 use shade_eval_print_loop::init;
-use steel::{
-    SteelVal,
-    steel_vm::{engine::Engine, register_fn::RegisterFn},
-};
 
 fn main() {
     init();
